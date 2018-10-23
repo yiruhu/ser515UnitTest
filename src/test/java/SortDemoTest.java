795292514f4911729dff41ef62f7a3c4b65d0bd4
yiruhu <35351043+yiruhu@users.noreply.github.com>
2018-10-22 21:38:30 -0700

    Update SortDemoTest.java

--- src/test/java/SortDemoTest.java
+++ src/test/java/SortDemoTest.java
@@ -76,10 +76,10 @@ public class SortDemoTest{
 	    
 	    SortDemoData data2 = new SortDemoData();
 	    data2.initializeArray("5 64 4");
-		assertTrue(data.myArray.length == 3);
+		assertTrue(data.myArray.length == 5);
 		assertTrue(data.myArray[0].key == data2.myArray[0].key );
-		assertTrue(data.myArray[1].key == data2.myArray[1].key );
-		assertTrue(data.myArray[2].key == data2.myArray[2].key );
+		assertTrue(data.myArray[1].key == data2.myArray[0].key );
+		assertTrue(data.myArray[2].key == data2.myArray[1].key );
 	}
 
 	
